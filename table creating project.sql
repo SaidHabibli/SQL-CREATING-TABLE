f35f9e256ffd6661809a96e421b44010eb533ba2
pewpewtender <hebibliseid99@gmail.com>
2024-04-23 16:31:09 +0300

    add comments to creating tables sql

--- table creating project.sql
+++ table creating project.sql
@@ -1,3 +1,4 @@
+-- firstly creating main table called 'EMPLOYEE' 
 CREATE TABLE employee(
     emp_id INT PRIMARY KEY,
     first_name VARCHAR(40),
@@ -9,6 +10,7 @@ CREATE TABLE employee(
     branch_id INT
 );
 
+-- afterwards creating yet another table called 'branch' so we would have more complex dataset. Since i have 2 tables now i can add foreign key
 CREATE TABLE branch(
     branch_id INT PRIMARY KEY,
     branch_name TEXT ,
@@ -17,6 +19,7 @@ CREATE TABLE branch(
     FOREIGN KEY (mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL
 );
 
+-- adding more foreign keys
 ALTER TABLE employee
 ADD FOREIGN KEY (super_id)
 REFERENCES employee(emp_id)
@@ -27,6 +30,7 @@ ADD FOREIGN KEY (branch_id)
 REFERENCES branch(branch_id)
 ON DELETE SET NULL;
 
+--creating 3 more tables which are called 'client','works_with','branch_supplier' and assigning more foreign keys in order to have complex dataset to work with
 CREATE TABLE client(
     client_id INT PRIMARY KEY,
     client_name VARCHAR(40),
@@ -51,15 +55,18 @@ CREATE TABLE  branch_supplier(
     FOREIGN KEY (branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
 );
 
+--after creating all tables necessary , it is time for inserting datas, since 'employee' and ' branch' are heavily involved in eact other , should insert values accordingly , starting with the 1st values of 'employee' table
 INSERT INTO employee VALUES(100,'david','wallace','1967-11-17','m',250000,NULL,NULL);
+--then have to add 1st values of branch table then we can fill all the remaining datas
 INSERT INTO branch VALUES(1,'corporate','100','2006-02-09');
 
+--since branch_id for 1st employee was not filled because we didnt had necessary branch information in place we should update our table
 UPDATE employee
 SET branch_id = '1'
 WHERE emp_id = '100';
-
+--filling rest of the employees from branch 1
 INSERT INTO employee VALUES(101, 'Jan', 'Levinson', '1961-05-11', 'F', 110000, 100, 1);
-
+-- will proceed to do the same for branch 2
 INSERT INTO employee VALUES(102, 'Michael', 'Scott', '1964-03-15', 'M', 75000, 100, NULL);
 INSERT INTO branch VALUES(2, 'Scranton', 102, '1992-04-06');
 
@@ -70,7 +77,7 @@ WHERE emp_id = 102;
 INSERT INTO employee VALUES(103, 'Angela', 'Martin', '1971-06-25', 'F', 63000, 102, 2);
 INSERT INTO employee VALUES(104, 'Kelly', 'Kapoor', '1980-02-05', 'F', 55000, 102, 2);
 INSERT INTO employee VALUES(105, 'Stanley', 'Hudson', '1958-02-19', 'M', 69000, 102, 2);
-
+-- finally branch 3
 INSERT INTO employee VALUES(106, 'Josh', 'Porter', '1969-09-05', 'M', 78000, 100, NULL);
 INSERT INTO branch VALUES(3, 'Stamford', 106, '1998-02-13');
 
@@ -80,7 +87,7 @@ WHERE emp_id = 106;
 
 INSERT INTO employee VALUES(107, 'Andy', 'Bernard', '1973-07-22', 'M', 65000, 106, 3);
 INSERT INTO employee VALUES(108, 'Jim', 'Halpert', '1978-10-01', 'M', 71000, 106, 3);
-
+--values for branch_supplier table
 INSERT INTO branch_supplier VALUES(2, 'Hammer Mill', 'Paper');
 INSERT INTO branch_supplier VALUES(2, 'Uni-ball', 'Writing Utensils');
 INSERT INTO branch_supplier VALUES(3, 'Patriot Paper', 'Paper');
@@ -88,7 +95,7 @@ INSERT INTO branch_supplier VALUES(2, 'J.T. Forms & Labels', 'Custom Forms');
 INSERT INTO branch_supplier VALUES(3, 'Uni-ball', 'Writing Utensils');
 INSERT INTO branch_supplier VALUES(3, 'Hammer Mill', 'Paper');
 INSERT INTO branch_supplier VALUES(3, 'Stamford Lables', 'Custom Forms');
-
+--values for client table
 INSERT INTO client VALUES(400, 'Dunmore Highschool', 2);
 INSERT INTO client VALUES(401, 'Lackawana Country', 2);
 INSERT INTO client VALUES(402, 'FedEx', 3);
@@ -96,7 +103,7 @@ INSERT INTO client VALUES(403, 'John Daly Law, LLC', 3);
 INSERT INTO client VALUES(404, 'Scranton Whitepages', 2);
 INSERT INTO client VALUES(405, 'Times Newspaper', 3);
 INSERT INTO client VALUES(406, 'FedEx', 2);
-
+--values for works_with table
 INSERT INTO works_with VALUES(105, 400, 55000);
 INSERT INTO works_with VALUES(102, 401, 267000);
 INSERT INTO works_with VALUES(108, 402, 22500);
@@ -107,7 +114,7 @@ INSERT INTO works_with VALUES(107, 405, 26000);
 INSERT INTO works_with VALUES(102, 406, 15000);
 INSERT INTO works_with VALUES(105, 406, 130000);
 
-
+--confirmation
 SELECT*FROM employee;
 SELECT*FROM branch;
 SELECT*FROM client;
